a9b695aa554404b158b019990371bc34ef5252f7
Your Name <you@example.com>
2020-07-11 23:29:48 +0530

    first commit

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerTogggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+
+const toolbar = (props) => (
+    <header className={classes.Toolbar}>
+    <DrawerTogggle clicked={props.drawerToggleClicked} />
+    <div className={classes.Logo}>
+    <Logo />
+    </div>
+    
+    <nav className={classes.DesktopOnly}>
+        <NavigationItems />
+    </nav>
+    </header>
+);
+
+export default toolbar;
